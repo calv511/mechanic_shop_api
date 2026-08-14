32196bf2fa67cc6fed587df467b7e06ffb269bb8
Carlos Alvarez <carlosalvarez214@gmail.com>
2026-08-14 16:26:13 -0400

    Refactor app.py to initialize Marshmallow and define CustomerSchema for serialization

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import String
+from flask_marshmallow import Marshmallow
 from datetime import date
 from dotenv import load_dotenv
 import os
@@ -17,12 +18,14 @@ db_host = os.getenv("DB_HOST")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://root:{db_pass}@localhost/{db_name}'
 
-class Base(DeclarativeBase):
-    pass
-
 db = SQLAlchemy(model_class= Base)
+ma = Marshmallow()
 
 db.init_app(app)
+ma.init_app(app)
+
+class Base(DeclarativeBase):
+    pass
 
 class Customer(Base):
     __tablename__ = 'customers'
@@ -67,4 +70,13 @@ class Mechanic(Base):
         secondary=service_mechanics,
         back_populates='mechanics'
     )
+
+class CustomerSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Customer # using the SQLAlchemy model to create fields used in
+                       # serialization, deserialization, and validation
+        
+        customer_schema = CustomerSchema()
+        customers_schema = CustomerSchema(many=True) # variant that allows for the serialization of many Users
+    
 app.run()
