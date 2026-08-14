5e495613019e8bfe26634ac7d56438fc6e0f2772
Carlos Alvarez <carlosalvarez214@gmail.com>
2026-08-13 21:40:32 -0400

    Add Customers model with fields for id, name, email, and phone

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String
+from datetime import date
 from dotenv import load_dotenv
 import os
+from typing import List
 
 load_dotenv()
 
@@ -20,5 +23,12 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class= Base)
 
 db.init_app(app)
+
+class Customers(Base):
+    __tablename__ = 'customers'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(db.String(255), nullable=False)
+    email: Mapped[str] = mapped_column(db.String(360), nullable=False, unique=True)
+    phone: Mapped[str] = mapped_column(db.String(255), nullable=False, unique=True)
 
 app.run()
