aca3b7d570a22e0f72f668d0a99acde6ac6e8e7f
Carlos Alvarez <carlosalvarez214@gmail.com>
2026-08-13 22:07:58 -0400

    Refactor relationships in Customer and Service_Ticket models; add Mechanic model with relationships to Service_Ticket

--- app.py
+++ app.py
@@ -31,7 +31,15 @@ class Customer(Base):
     email: Mapped[str] = mapped_column(db.String(360), nullable=False, unique=True)
     phone: Mapped[str] = mapped_column(db.String(255), nullable=False, unique=True)
 
-    services_tickets: Mapped[List['Service_Ticket']]
+    services_tickets: Mapped[List['Service_Ticket']] = db.relationship(back_populates='customer')
+
+# Junction
+service_mechanics = db.Table(
+    'service_mechanics',
+    Base.metadata,
+    db.Column('ticket_id', db.ForeignKey('service_tickets.id'), primary_key=True),
+    db.Column('mechanic_id', db.ForeignKey('mechanics.id'), primary_key=True)
+)
 
 class Service_Ticket(Base):
     __tablename__ = 'service_tickets'
@@ -41,12 +49,22 @@ class Service_Ticket(Base):
     service_desc: Mapped[str] = mapped_column(db.String(255))
     customer_id: Mapped[int] = mapped_column(db.ForeignKey('customers.id'))
 
-    customer: Mapped['Customer'] = db.relationship(back_populates='service_ticket')
+    customer: Mapped['Customer'] = db.relationship(back_populates='services_tickets')
+    mechanics: Mapped[List['Mechanic']] = db.relationship(
+        secondary=service_mechanics,
+        back_populates='service_tickets'
+    )
+
 
 class Mechanic(Base):
+    __tablename__ = 'mechanics'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(db.String(255))
     email: Mapped[str] = mapped_column(db.String(255))
     phone: Mapped[str] = mapped_column(db.String(255))
     salary: Mapped[float] = mapped_column()
+    service_tickets: Mapped[List['Service_Ticket']] = db.relationship(
+        secondary=service_mechanics,
+        back_populates='mechanics'
+    )
 app.run()
