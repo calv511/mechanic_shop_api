41cde1892664a02e65d496fe05e971f7b5693fa4
Carlos Alvarez <carlosalvarez214@gmail.com>
2026-08-13 21:56:12 -0400

    Refactor Customer class and add Service_Ticket model with relationships

--- app.py
+++ app.py
@@ -24,11 +24,22 @@ db = SQLAlchemy(model_class= Base)
 
 db.init_app(app)
 
-class Customers(Base):
+class Customer(Base):
     __tablename__ = 'customers'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(db.String(255), nullable=False)
     email: Mapped[str] = mapped_column(db.String(360), nullable=False, unique=True)
     phone: Mapped[str] = mapped_column(db.String(255), nullable=False, unique=True)
 
+    services_tickets: Mapped[List['Service_Ticket']]
+
+class Service_Ticket(Base):
+    __tablename__ = 'service_tickets'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    VIN: Mapped[str] = mapped_column(db.String(20))
+    service_date: Mapped[date] = mapped_column(db.Date)
+    service_desc: Mapped[str] = mapped_column(db.String(255))
+    customer_id: Mapped[int] = mapped_column(db.ForeignKey('customers.id'))
+
+    customer: Mapped['Customer'] = db.relationship(back_populates='service_ticket')
 app.run()
