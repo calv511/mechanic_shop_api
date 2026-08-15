912aa39734c6510f3faaf8ab33911a883763d352
Carlos Alvarez <carlosalvarez214@gmail.com>
2026-08-15 18:08:23 -0400

    Add configuration classes for different environments: Development, Testing, and Production

---
+++ config.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+db_pass = os.getenv("DB_PASS")
+db_name = os.getenv("DB_NAME")
+db_host = os.getenv("DB_HOST")
+
+class DevelopmentConfig:
+    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://root:{db_pass}@localhost/{db_name}'
+    DEBUG = True
+
+class TestingConfig:
+    pass
+
+class ProductionConfig:
+    pass
