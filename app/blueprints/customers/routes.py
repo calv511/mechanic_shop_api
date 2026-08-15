693ed9bf2ee3ac927cbde8065b43fd066fb59237
Carlos Alvarez <carlosalvarez214@gmail.com>
2026-08-15 18:37:50 -0400

    Refactor customer management routes and schemas

---
+++ app/blueprints/customers/routes.py
@@ -0,0 +1,71 @@
+from .schemas import customer_schema, customers_schema
+from flask import request, jsonify
+from marshmallow import ValidationError
+from sqlalchemy import select
+from app.models import Customer, db
+from . import customers_bp as app
+
+@app.route("/customers", methods=['POST'])
+def create_customer():
+    try:
+        customer_data = customer_schema.load(request.get_json())
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+
+    existing_customer = db.session.execute(
+        select(Customer).where(Customer.email == customer_data['email'])
+    ).scalars().first()
+
+    if existing_customer:
+        return jsonify({"error": "Email already associated with an account."}), 400
+
+    new_customer = Customer(**customer_data)
+    db.session.add(new_customer)
+    db.session.commit()
+    return customer_schema.jsonify(new_customer), 201
+
+# GET ALL CUSTOMERS
+@app.route("/customers", methods=['GET'])
+def get_customers():
+    customers = db.session.execute(select(Customer)).scalars().all()
+    return customers_schema.jsonify(customers)
+
+# GET SPECIFIC CUSTOMER
+@app.route("/customers/<int:customer_id>", methods=['GET'])
+def get_customer(customer_id):
+    customer = db.session.get(Customer, customer_id)
+
+    if customer:
+        return customer_schema.jsonify(customer), 200
+    return jsonify({"error": "Customer not found."}), 404
+
+# UPDATE SPECIFIC USER
+@app.route("/customers/<int:customer_id>", methods=["PUT"])
+def update_customer(customer_id):
+    customer = db.session.get(Customer, customer_id)
+
+    if not customer:
+        return jsonify({"error": "Customer not found."}), 404
+
+    try:
+        customer_data = customer_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+
+    for key, value in customer_data.load(request.json):
+        setattr(customer, key, value)
+
+    db.session.commit()
+    return customer_schema.jsonify(customer), 200
+
+# DELETE SPECIFIC CUSTOMER
+@app.route("/customers/<int:customer_id>", methods=["DELETE"])
+def delete_customer(customer_id):
+    customer = db.session.get(Customer, customer_id)
+
+    if not customer:
+        return jsonify({"error": "Customer not found."}), 404
+
+    db.session.delete(customer)
+    db.session.commit()
+    return jsonify({"message": f'Customer id: {customer_id}, successfully deleted.'}), 200
