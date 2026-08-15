c2d26c71d975524096ead184399ee481e67c04e3
Carlos Alvarez <carlosalvarez214@gmail.com>
2026-08-15 18:26:45 -0400

    Add customers blueprint and register with the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .extensions import ma
 from .models import db
+from blueprints.customers import customers_bp
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -11,5 +12,6 @@ def create_app(config_name):
     db.init_app(app)
 
     # Register Blueprints
+    app.register_blueprint(customers_bp, url_prefix='/customers')
 
     return app
