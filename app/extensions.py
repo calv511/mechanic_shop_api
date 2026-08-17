93e4593c63742a32a6aea31ee270e55523495e8e
Carlos Alvarez <carlosalvarez214@gmail.com>
2026-08-17 15:45:22 -0400

    Refactor Limiter initialization to include default rate limits

--- app/extensions.py
+++ app/extensions.py
@@ -2,6 +2,9 @@ from flask_marshmallow import Marshmallow
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
-limiter = Limiter(key_func=get_remote_address) # creating an instance of Limiter
+limiter = Limiter(                # creating an instance of Limiter
+    key_func=get_remote_address,
+    default_limits=["100 per day", "20 per hour"]
+    )
 
 ma = Marshmallow() 
