1a8ddbad98d0770f3b35d3357909eeac042fc6b7
Carlos Alvarez <carlosalvarez214@gmail.com>
2026-08-18 02:50:28 -0400

    Refactor token encoding function and implement token validation decorator

--- app/utils/util.py
+++ app/utils/util.py
@@ -3,17 +3,44 @@ from jose import jwt
 import jose
 from dotenv import load_dotenv
 import os
-
+from functools import wraps
+from flask import request, jsonify
 load_dotenv
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-def encode_token(user_id):
+def encode_token(customer_id):
     payload = {
         'exp': datetime.now(timezone.utc) + timedelta(days=0, hours=1), # Set expiration time
         'iat': datetime.now(timezone.utc), # Issues at
-        'sub': user_id 
+        'sub': customer_id 
     }
 
     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
     return token
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if "Authorization" in request.headers:
+            token = request.headers['Authorization'].split()[1]
+
+            if not token:
+                return jsonify({"message": "missing token"}), 400
+
+            try:
+                data = jwt.decode(token, SECRET_KEY, algorithms="HS256")
+                customer_id = data['sub']
+            except jwt.ExpiredSignatureError as e:
+                return jsonify({"message": "token expired"}), 400
+            except jwt.InvalidTokenError:
+                return jsonify({"message": "invalid token"}), 400
+
+            return f(customer_id, *args, **kwargs)
+
+        else:
+            return jsonify({"message": "You must be logged in to accces this"}), 400
+
+    return decorated
