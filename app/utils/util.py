d1eab31a695aa6df4c4ebac984e621cc3a556b84
Carlos Alvarez <carlosalvarez214@gmail.com>
2026-08-18 00:26:10 -0400

    Add utility functions for JWT token encoding

---
+++ app/utils/util.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+import jose
+from dotenv import load_dotenv
+import os
+
+load_dotenv
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+def encode_token(user_id):
+    payload = {
+        'exp': datetime.now(timezone.utc) + timedelta(days=0, hours=1) # Set expiration time
+        'iat': datetime.now(timezone.utc) # Issues at
+        'sub': user_id 
+    }
+
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    return token
