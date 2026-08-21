f5f357addefe85243b98605b2cbbc67c9ff1b84f
Carlos Alvarez <carlosalvarez214@gmail.com>
2026-08-21 15:01:18 -0400

    Refactor customer tests to include setup for customer instance and add login test case

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,20 +1,31 @@
 from app import create_app
-from app.models import db
+from app.models import db, Customer
+from app.utils.util import encode_token
+from werkzeug.security import generate_password_hash
 import unittest
 
 class TestCustomer(unittest.TestCase):
     def setUp(self):
         self.app = create_app("TestingConfig")
+        self.customer = Customer(
+            name="test_user",
+            email="test@email.com",
+            phone="555-111-1111",
+            password=generate_password_hash('test')
+        )
         with self.app.app_context():
             db.drop_all()
             db.create_all()
+            db.session.add(self.customer)
+            db.session.commit()
+        self.token = encode_token(1)
         self.client = self.app.test_client()
 
     def test_create_customer(self):
         customer_payload = {
             "name": "John Doe",
             "email": "jd@email.com",
-            "phone": "555-555-5555",
+            "phone": "555-555-5556",
             "password": "123"
         }
 
@@ -32,3 +43,13 @@ class TestCustomer(unittest.TestCase):
         response = self.client.post('/customers/', json=customer_payload)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json['email'], ['Missing data for required field.'])
+
+    def test_login_customer(self):
+        credentials = {
+            "email": "test@email.com",
+            "password": "test"
+        }
+
+        response = self.client.post('/customers/login', json=credentials)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['status'], 'success')
